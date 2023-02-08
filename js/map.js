a06a05241c18cd927c8503cb1370b7f87d59eff7
Yanis Rezgui <chayano57@gmail.com>
2023-02-08 10:23:39 +0100

    map

--- js/map.js
+++ js/map.js
@@ -11,30 +11,28 @@ app.renderer.resize(window.innerWidth, window.innerHeight)
 app.renderer.view.style.position = 'absolute'
 document.body.appendChild(app.view);
 
-const Graphics = PIXI.Graphics
 
-const rectangle = new Graphics();
-rectangle.beginFill(0xAA33BB)
-    .lineStyle(4, 0xFFEA00, 1)
-    .drawRect(200, 200, 100, 120)
-    .endFill();
+const char1sprite = new PIXI.Sprite.from('../images/robot.png')
 
-app.stage.addChild(rectangle)
+app.stage.addChild(char1sprite)
 
-const style = new PIXI.TextStyle({
-    fontFamily: 'Montserrat',
-    fontSize: 48,
-    fill: 'deepskyblue',
-    stroke: '#ffffff',
-    strokeThickness: 4,
-    dropShadow: true,
-    dropShadowDistance: 10,
-    dropShadowAngle: Math.PI / 2,
-    dropShadowBlur: 4,
-    dropShadowColor: '#000000'
-});
+char1sprite.width = 50
+char1sprite.height = 50
 
-const mytext = new PIXI.Text('hello World', style)
 
-app.stage.addChild(mytext)
-mytext.text = 'text changed'
+/* char1sprite.x = 400
+char1sprite.y = 400 */
+char1sprite.position.set(500, 400)
+char1sprite.anchor.set(0.5, 0.5)
+
+app.ticker.add(delta => loop(delta))
+function loop(delta) {
+    /* char1sprite.x += 1
+    char1sprite.y += -0.4 */
+/*     char1sprite.rotation += 0.01
+ */}
+
+char1sprite.interactive = true;
+char1sprite.on('pointerdown', function () {
+    char1sprite.position.set(Math.floor(window.innerWidth * Math.random()), window.innerHeight * Math.random())
+})
