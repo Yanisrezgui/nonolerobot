879a69e5d5025ea6417734a6dc3f421900a7f55a
Yanis Rezgui <chayano57@gmail.com>
2023-04-11 20:52:11 +0200

    j'ai ca et c deja bien

--- Application/js/globals.js
+++ Application/js/globals.js
@@ -1,29 +1,29 @@
 const robot_speed = 500;
-const act_rate    = 5;
+const act_rate = 5;
 
 const sensor_range_default = 100;
-let sensor_range   = 150;
-const sensor_fov     = Math.PI/6;
-const sensor_angles  = [ -Math.PI/8, Math.PI/8];
+let sensor_range = 150;
+const sensor_fov = Math.PI / 6;
+const sensor_angles = [-Math.PI / 8, Math.PI / 8];
 
 let paused = false;
 const bias_value = 1;
 
-const num_cherries  = 0;
-const num_obstacles =  30;
-      
+const num_cherries = 50;
+const num_obstacles = 30;
+
 let app;
 
-const textures     = {};
-const cherries     = [];
-const obstacles    = [];
+const textures = {};
+const cherries = [];
+const obstacles = [];
 
 let nono = null;
 
 let elapsed = 0.0;
-let tics    = 0;
-let score   = 0;
-let battery   = 100;
+let tics = 0;
+let score = 0;
+let battery = 100;
 
 
 const debug = false; // fix the number of cherries to 1
