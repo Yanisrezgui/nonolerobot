04afa913f69cfc20e57cc43538a96747984d55c2
Yanis Rezgui <chayano57@gmail.com>
2023-04-11 18:13:33 +0200

    vie de nono

--- Application/js/home.js
+++ Application/js/home.js
@@ -1,11 +1,11 @@
-document.querySelector("#newGame").addEventListener("click", ()=>{
+document.querySelector("#newGame").addEventListener("click", () => {
     document.getElementById("newGame").style.display = "none";
     document.querySelector(".text-warning").style.display = "none";
     document.getElementById("infos").style.display = "none";
     document.getElementById("about").style.display = "none";
 })
 
-document.querySelector("#about").addEventListener("click", ()=>{
+document.querySelector("#about").addEventListener("click", () => {
     document.querySelector(".text-warning").style.display = "none";
     document.getElementById("about").style.display = "none";
     document.getElementById("newGame").style.display = "none";
@@ -13,7 +13,7 @@ document.querySelector("#about").addEventListener("click", ()=>{
     document.getElementById("back").style.display = "block";
 })
 
-document.querySelector("#back").addEventListener('click', ()=>{
+document.querySelector("#back").addEventListener('click', () => {
     document.getElementById("back").style.display = "none";
     document.getElementById("infos").style.display = "none";
     document.querySelector(".text-warning").style.display = "block";
--- Application/js/main.js
+++ Application/js/main.js
@@ -1,51 +1,51 @@
-function demo_init(){
+function demo_init() {
 
-    // check for WebGL
-    
-    let type = "WebGL";
-    if (!PIXI.utils.isWebGLSupported()) {
-	type = "canvas";
-    }
-    PIXI.utils.sayHello(type);
-    
-    // Create a Pixi Application
-    
-    app = new PIXI.Application({
-	width: window.innerWidth * 2 /4,      	// default: 800
-	height: window.innerHeight, 			// default: 600
-	antialias: true,    					// default: false
-	transparent: false, 					// default: false
-	resolution: 1,      					// default: 1
-	//autoResize: true
-    });
+	// check for WebGL
 
-    // Load ressources (images for sprites)
-    
-    PIXI.Loader.shared
-      .add('robot', "../assets/nono-small.png")
-      .add('cherry', "../assets/cherry.png")
-	  .add('obstacle', "../assets/obstacle.png")
-      .load(( loader, resources ) => {
-	    
-	  //make sprites 
+	let type = "WebGL";
+	if (!PIXI.utils.isWebGLSupported()) {
+		type = "canvas";
+	}
+	PIXI.utils.sayHello(type);
 
-	  textures.robot = resources.robot.texture;
-	  textures.cherry = resources.cherry.texture;
-	  textures.obstacle = resources.obstacle.texture;
-	  
-	  
-	  // when finished loading images, start everyting else
+	// Create a Pixi Application
 
-	  demo_start();
-		
+	app = new PIXI.Application({
+		width: window.innerWidth * 2 / 4,      	// default: 800
+		height: window.innerHeight, 			// default: 600
+		antialias: true,    					// default: false
+		transparent: false, 					// default: false
+		resolution: 1,      					// default: 1
+		//autoResize: true
 	});
+
+	// Load ressources (images for sprites)
+
+	PIXI.Loader.shared
+		.add('robot', "../assets/nono-small.png")
+		.add('cherry', "../assets/cherry.png")
+		.add('obstacle', "../assets/obstacle.png")
+		.load((loader, resources) => {
+
+			//make sprites 
+
+			textures.robot = resources.robot.texture;
+			textures.cherry = resources.cherry.texture;
+			textures.obstacle = resources.obstacle.texture;
+
+
+			// when finished loading images, start everyting else
+
+			demo_start();
+
+		});
 }
 
 function demo_start() {
-  
-    // Add the canvas (app.view) to the HTML document
-	window.onresize = function (){
-		let w = window.innerWidth * 2 /3;
+
+	// Add the canvas (app.view) to the HTML document
+	window.onresize = function () {
+		let w = window.innerWidth * 2 / 3;
 		let h = window.innerHeight;
 
 		//this part resizes the canvas but keeps ratio the same
@@ -59,64 +59,64 @@ function demo_start() {
 	document.getElementById("carte").appendChild(app.view);
 
 
-    // Set the background
+	// Set the background
 
-    app.renderer.backgroundColor = 0x000000;
+	app.renderer.backgroundColor = 0x000000;
 
 	const herbe = new PIXI.Sprite.from('../assets/herbe.png')
-    app.stage.addChild(herbe)
+	app.stage.addChild(herbe)
 
-    herbe.width = window.innerWidth
-    herbe.height = window.innerHeight
-    herbe.opacity = 0.5
-           
-    if (! debug ){
-	
-	// Add food and a robot 
-	make_cherries(num_cherries, app.stage) ;
-	make_robot(app.stage);
-	make_obstacle(num_obstacles, app.stage);
+	herbe.width = window.innerWidth
+	herbe.height = window.innerHeight
+	herbe.opacity = 0.5
+
+	if (!debug) {
+
+		// Add food and a robot 
+		make_cherries(num_cherries, app.stage);
+		make_robot(app.stage);
+		make_obstacle(num_obstacles, app.stage);
 
 	}
-    else {
+	else {
 		//DEBUG
 
 		let obj = new PIXI.Graphics();
 		obj.beginFill(0xff0000);
-		obj.drawRect(0, 0,  100 + sensor_range * 0.5,  100 + 20);
+		obj.drawRect(0, 0, 100 + sensor_range * 0.5, 100 + 20);
 		app.stage.addChild(obj);
 		obj = new PIXI.Graphics();
 		obj.beginFill(0x0000ff);
 		obj.drawRect(0, 0, 100, 100);
 		app.stage.addChild(obj);
 
-		let cherry =  new Cherry(0,100+sensor_range * 0.5, 120, 0 );
-		cherries.push ( cherry );
-		app.stage.addChild( cherry );
+		let cherry = new Cherry(0, 100 + sensor_range * 0.5, 120, 0);
+		cherries.push(cherry);
+		app.stage.addChild(cherry);
 
-		let obst =  new Obstacle(0,100+sensor_range * 0.5, 80, 0 );
-		obstacles.push (obst );
-		app.stage.addChild( obst );
-      
-		nono = new Robot(100,100,0, sensor_range_default);
-		app.stage.addChild( nono );
-    
-    }
-    paused = true;
+		let obst = new Obstacle(0, 100 + sensor_range * 0.5, 80, 0);
+		obstacles.push(obst);
+		app.stage.addChild(obst);
 
-    app.ticker.add((delta) => game_loop(delta));
+		nono = new Robot(100, 100, 0, sensor_range_default);
+		app.stage.addChild(nono);
+
+	}
+	paused = true;
+
+	app.ticker.add((delta) => game_loop(delta));
 	//app.ticker.stop();
 
 	// Add the event to the button
 	init_events();
-    
+
 }
 
 function init_events() {
 	let start = document.querySelector("#start");
 	start.onclick = function () {
 		//console.log(paused);
-		if(!paused){
+		if (!paused) {
 			nono.set_nn_parameter(get_nn_parameter())
 			//app.ticker.start();
 
@@ -124,7 +124,7 @@ function init_events() {
 			start.classList.remove("bi-pause-circle");
 			start.classList.add('bi-play-circle');
 
-		}else{
+		} else {
 			//app.ticker.stop();
 			//change icon of button
 			start.classList.remove("bi-play-circle");
@@ -134,42 +134,42 @@ function init_events() {
 	}
 
 	//console.log(nono.sensor_range);
-	document.querySelector("#reset").addEventListener("click", ()=>{
-		if(sensor_range_default!=sensor_range){
+	document.getElementById("reset").addEventListener("click", () => {
+		if (sensor_range_default != sensor_range) {
 			app.stage.removeChild(nono);
-			nono = new Robot(100,100,0, sensor_range_default);
-			app.stage.addChild( nono );
+			nono = new Robot(100, 100, 0, sensor_range_default);
+			app.stage.addChild(nono);
 			let vision = document.getElementById("vision");
 			vision.value = 100;
 
-		
-		}else{
+		} else {
 			app.stage.removeChild(nono);
-			nono = new Robot(100,100,0, sensor_range_default);
-			app.stage.addChild( nono );
+			nono = new Robot(100, 100, 0, sensor_range_default);
+			app.stage.addChild(nono);
 		}
 
 		nono.reset();
 
-		for (let i=0; i<num_cherries; i++) {
+		for (let i = 0; i < num_cherries; i++) {
 			cherries[i].relocate();
 		}
-		for (let i=0; i<num_obstacles; i++) {
+		for (let i = 0; i < num_obstacles; i++) {
 			obstacles[i].relocate();
 		}
 
+		NonoLife = 101
 		elapsed = 0;
-		tics 	= 0;
-		paused 	= true;
+		tics = 0;
+		paused = false;
 
-		
+
 	})
 
-	document.getElementById("update").addEventListener("click", ()=>{
+	document.getElementById("update").addEventListener("click", () => {
 		nono.set_nn_parameter(get_nn_parameter());
 		app.stage.removeChild(nono);
-		nono = new Robot(100,100,0, nono.reset_sensor());
-		app.stage.addChild( nono );
+		nono = new Robot(100, 100, 0, nono.reset_sensor());
+		app.stage.addChild(nono);
 		nono.reset();
 	})
 
@@ -183,9 +183,7 @@ function game_loop(delta) {
 		let dt = delta / 60;
 		elapsed += dt;
 		tics++;
-
-
-		let currentTime = elapsed/4;
+		let currentTime = elapsed / 4;
 		//console.log(elapsed);
 		let totalTime = 100;
 		let rate = currentTime / totalTime;
@@ -224,6 +222,25 @@ function game_loop(delta) {
 
 
 
+let NonoLife = 101;
 
+// déclaration d'une fonction pour faire perdre de la vie à Nono
+function LoseLife() {
+	// vérification si le jeu n'est pas en pause
+	if (NonoLife < 1) {
+		app.stop();
+		return
+	}
+	if (!paused) {
+		// soustraire 1 de la vie de Nono
+		NonoLife--;
+		// mettre à jour la barre de vie de Nono dans le HTML
+		document.getElementById('myBar').style.width = NonoLife + '%';
+		document.getElementById('myBar').innerHTML = NonoLife + '%';
+	}
+}
+
+// appel de la fonction LoseLife toutes les 2 secondes
+setInterval(LoseLife, 2000);
 
 
